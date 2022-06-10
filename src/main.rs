ed8dc4d85d7f5187917bd49afbc4a980b113249c
Divyank Aggarwal <divyank.aggarwal@remotestate.com>
2022-06-10 16:52:09 +0530

    added basic middlewares and logger

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ mod handlers;
 mod helpers;
 mod models;
 
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{middleware, web, App, HttpServer};
 use dotenv::dotenv;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::env;
@@ -17,6 +18,8 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env::set_var("RUST_BACKTRACE", "full");
     env::set_var("TEST", "1");
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPoolOptions::new()
         .max_connections(10)
@@ -25,10 +28,28 @@ async fn main() -> std::io::Result<()> {
         .expect("TESTING");
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allowed_methods(vec![
+                "GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH",
+            ])
+            .allowed_headers(vec![
+                "authorization",
+                "Origin",
+                "X-Requested-With",
+                "Content-Type",
+                "Accept",
+                "X-Access-Token",
+                "x-api-key",
+            ])
+            .send_wildcard()
+            .supports_credentials();
+
         App::new()
             .app_data(web::Data::new(DbState {
                 db_pool: pool.clone(),
             }))
+            .wrap(cors)
+            .wrap(middleware::Logger::default())
             .service(handlers::get_pizzas)
             .service(handlers::put_pizza)
             .service(handlers::get_order)
